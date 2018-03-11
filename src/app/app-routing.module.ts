a65a67732a4ce0ec37043569a351188aa45821dc
Rahul Bhammarker <Rahul Bhammarker>
2018-03-11 14:56:56 +0530

    Added summary component and few imporvements in survye component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SurveyComponent } from './survey/survey.component';
+import { SummaryComponent } from './summary/summary.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/survey', pathMatch: 'full' },
   { path: 'survey', component: SurveyComponent },
+  { path: 'summary', component: SummaryComponent },
   { path: 'survey/:questId', component: SurveyComponent }
 ];
 
---
+++ src/app/summary/summary.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SurveyService } from '../survey/survey.service';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent implements OnInit {
+  questionsAttempted;
+  surveyQuestions;
+
+  constructor(private surveyService: SurveyService) { }
+
+  ngOnInit() {
+    this.surveyQuestions = this.surveyService.getSuveyQuestions();
+    this.calculateQuestionAnswered();
+  }
+
+  calculateQuestionAnswered() {
+    this.questionsAttempted = 0;
+    this.surveyQuestions.forEach(element => {
+      if (!!element.answer) {
+        this.questionsAttempted++;
+      }
+    });
+  }
+
+}
