4c7b36a87698f8b77f5ae179d9bb836ba1ada63f
Rahul Bhammarker <Rahul Bhammarker>
2018-03-12 01:00:06 +0530

    Updated seed data and Readme for the same

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ This project is setup with the automated documentaion.
 To generate the automated documents run `npm run generate-docs` this will generate the project documents under the `documentation/` directory.
 
 Generated documentation can be seen by launching `index.html` from generated coverage folder in your browser. You can also serve documentation on local server just run `npm run serve-docs` and open : `http://127.0.0.1:8080`
+
+
+## Project Seed Mock Data
+This project uses the mock data which is used to bootstrap the application. The seed data can be found in `src/assets/survey-mock.ts` file.
+Feel free to manuplate this file to get the desired result of survey questions.
--- src/assets/survey-mock.ts
+++ src/assets/survey-mock.ts
@@ -1,26 +1,36 @@
 const surveyQuestions = [
   {
     id: 1,
-    question: 'Sample Question 1',
+    question: 'This is Sample Question 1',
     ansType: 'text',
-    answer: '',
-    isAnswered: ''
+    answer: ''
   },
   {
     id: 2,
-    question: 'Sample Question 2 with Radio Option',
+    question: 'This is Sample Question 2 with Radio Option',
     ansType: 'radio',
-    options: ['Radio Option 1', 'Radio Option 2'],
-    answer: '',
-    isAnswered: ''
+    options: ['Radio Option 1', 'Radio Option 2', 'Radio Option 3'],
+    answer: ''
   },
   {
     id: 3,
-    question: 'Sample Question 3 with Dropdown options',
+    question: 'This is Sample Question 3 with Dropdown Options',
     ansType: 'dropdown',
-    options: ['Drop down Option 1', 'Drop down Option 2'],
-    answer: '',
-    isAnswered: ''
+    options: ['Drop down Option 1', 'Drop down Option 2', 'Drop down Option 3'],
+    answer: ''
+  },
+  {
+    id: 4,
+    question: 'What is your Age ?',
+    ansType: 'text',
+    answer: ''
+  },
+  {
+    id: 5,
+    question: 'In which Country you were born ?',
+    ansType: 'dropdown',
+    options: ['USA', 'India', 'UK', 'Germany'],
+    answer: ''
   }
 ];
 
